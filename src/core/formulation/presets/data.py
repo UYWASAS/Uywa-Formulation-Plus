4ffa8e8d09a1a78ade9df0014a22bf984d90b204
UYWASAS <uywasas@gmail.com>
2026-07-08 10:37:25 -0500

    Create data.py

---
+++ src/core/formulation/presets/data.py
@@ -0,0 +1,10 @@
+"""
+Fuente única de presets nutricionales internos del módulo.
+Puedes pegar aquí el diccionario PRESETS completo sin depender de archivos raíz.
+"""
+
+PRESETS = {
+    "Aves": {},
+    "Cerdos": {},
+    "Rumiantes": {},
+}
